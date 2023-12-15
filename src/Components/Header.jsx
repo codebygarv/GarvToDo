e2e7c95e857a01f824470994b615ced4684b9ff1
Garv Thakral <144711031+codebygarv@users.noreply.github.com>
2023-12-15 15:27:46 +0530

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="bg-gray-600 text-white p-4 header">
+    </header>
+  );
+}
+
+export default Header;
